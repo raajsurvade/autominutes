9264b2043b7c42b4746344d21e2758726ad41c8e
Chirag <cschiragmate@gmail.com>
2017-10-07 13:11:13 +0530

    Initial check-in of module Minutes

---
+++ Minutes/MinutesPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Minutes
+{
+    public partial class MinutesPage : ContentPage
+    {
+        public MinutesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
